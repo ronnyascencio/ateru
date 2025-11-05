806c7be1665c9956176f03044872f31d1f13aaef
Ronny Ascencio <ronnycompartist@gmail.com>
2025-11-05 17:46:36 -0500

    create setup_env.sh for pipeline environment variable make it permanent in user workstation

---
+++ scripts/setup_env.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+PIPELINE_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
+export PIPELINE_ROOT
+echo "PIPELINE_ROOT set to: $PIPELINE_ROOT"
+
+# adding PATH temporal
+export PATH="$PIPELINE_ROOT/cli:$PATH"
+
+# make it permanent  ~/.bashrc if not exist
+if ! grep -q "PIPELINE_ROOT" ~/.bashrc; then
+    echo "export PIPELINE_ROOT=$PIPELINE_ROOT" >> ~/.bashrc
+    echo 'export PATH="$PIPELINE_ROOT/cli:$PATH"' >> ~/.bashrc
+    echo "Added PIPELINE_ROOT permanently to ~/.bashrc"
+fi
