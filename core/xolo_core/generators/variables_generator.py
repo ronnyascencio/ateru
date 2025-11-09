090122167fb3e2bbf4620072de219a4fd6584fcf
Ronny Ascencio <ronnycompartist@gmail.com>
2025-11-09 09:12:03 -0500

    Variable generator for PROJECT_ROOT, ASSET_ROOT, OCIO config for blender and nuke

--- core/xolo_core/generators/variables_generator.py
+++ core/xolo_core/generators/variables_generator.py
@@ -1,5 +1,9 @@
-from pathlib import Path
 import os
+from pathlib import Path
+
+from rich.console import Console
+
+console = Console()
 
 
 def project_root_variable(project_root: str):
@@ -8,7 +12,30 @@ def project_root_variable(project_root: str):
     assets_root_env = Path(root_project_folder, "assets").resolve()
     os.environ["PROJECT_ROOT"] = str(project_root_env)
     os.environ["ASSETS_ROOT"] = str(assets_root_env)
-    print(f"DEBUG: Set PROJECT_ROOT to {os.environ['PROJECT_ROOT']}")
-    print(f"DEBUG: Set ASSETS_ROOT to {os.environ['ASSETS_ROOT']}")
+    console.print(
+        f"DEBUG: Set PROJECT_ROOT to {os.environ['PROJECT_ROOT']}", style="green"
+    )
+    console.print(
+        f"DEBUG: Set ASSETS_ROOT to {os.environ['ASSETS_ROOT']}", style="green"
+    )
 
     return os.environ.get("PROJECT_ROOT"), os.environ.get("ASSETS_ROOT")
+
+
+def ocio_variable(dcc: str):
+    if dcc == "Blender":
+        ocio_path = (
+            Path(__file__).resolve().parent.parent.parent
+            / "ocio"
+            / "studio-config-all-views-v3.0.0_aces-v2.0_ocio-v2.4.ocio"
+        )
+    else:
+        ocio_path = (
+            Path(__file__).resolve().parent.parent.parent
+            / "ocio"
+            / "studio-config-v2.2.0_aces-v1.3_ocio-v2.3.ocio"
+        )
+
+    os.environ["OCIO"] = str(ocio_path)
+    console.print(f"DEBUG: Set OCO to {os.environ['OCIO']}", style="green")
+    return os.environ.get("OCIO")
