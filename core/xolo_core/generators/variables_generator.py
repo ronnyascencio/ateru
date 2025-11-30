731f4bfe4408c0e6a54c88d9f07398f0dfc1d6ec
Ronny Ascencio <ronnycompartist@gmail.com>
2025-11-29 21:59:16 -0500

    function to get core path of pipeline

--- core/xolo_core/generators/variables_generator.py
+++ core/xolo_core/generators/variables_generator.py
@@ -45,3 +45,8 @@ def prman_variable():
     opt_path = "/opt/pixar/RenderManProServer-27.0"
     prman_path = Path(opt_path).resolve()
     return str(prman_path)
+
+
+def core_path():
+    core_root = Path(__file__).resolve().parent.parent.parent.parent
+    return str(core_root)
