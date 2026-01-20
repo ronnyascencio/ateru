ccf235ce6ca11babd70caa20702fdbb502b6acb2
Ronny Ascencio <ronnycompartist@gmail.com>
2026-01-19 20:58:55 -0500

    fix(core):  list projects none return error fixed

--- core/xolo_core/project/delete.py
+++ core/xolo_core/project/delete.py
@@ -1,4 +1,3 @@
-
 from pathlib import Path
 import shutil
 from core.xolo_core.project.scan import list_projects
@@ -6,13 +5,9 @@ from core.xolo_core.config import loader
 from core.xolo_core.logging import events
 
 
-
 def project_delete(project_name: str):
-
-
-
-    project_dir: Path  =  loader.read_xolo_config()
-    projects: list | None = list_projects(project_dir)
+    project_dir: Path = loader.read_xolo_config()
+    projects: list = list_projects(project_dir) or []
 
     if project_name not in projects:
         events.error(f"Project ->{project_name}<- not found in Projects")
@@ -22,5 +17,4 @@ def project_delete(project_name: str):
         shutil.rmtree(project_to_delete)
         events.success(f"Project: {project_name}, deleted in root: {project_to_delete}")
     except Exception as e:
-
         events.error(f"Could not delete {project_name}: {str(e)}")
