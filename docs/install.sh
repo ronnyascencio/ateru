88973159c4d5bed99bf31a21cfccf3e251c2339f
Ronny Ascencio <ronnycompartist@gmail.com>
2026-03-22 16:59:38 -0400

    adding cuestion to user

--- docs/install.sh
+++ docs/install.sh
@@ -10,38 +10,49 @@ OS="$(uname)"
 echo "[+] Detected OS: $OS"
 
 
-if ! command -v git >/dev/null 2>&1; then
-    echo "[!] git not found. Installing..."
-    if [[ "$OS" == "Linux" ]]; then
-        if command -v apt >/dev/null 2>&1; then sudo apt update && sudo apt install -y git; fi
-        if command -v dnf >/dev/null 2>&1; then sudo dnf install -y git; fi
-    elif [[ "$OS" == "Darwin" ]]; then
-        brew install git
+for cmd in git curl; do
+    if ! command -v $cmd >/dev/null 2>&1; then
+        echo "[!] $cmd not found. Installing..."
+        if [[ "$OS" == "Linux" ]]; then
+            if command -v apt >/dev/null 2>&1; then sudo apt update && sudo apt install -y $cmd; fi
+            if command -v dnf >/dev/null 2>&1; then sudo dnf install -y $cmd; fi
+        elif [[ "$OS" == "Darwin" ]]; then
+            brew install $cmd
+        fi
+    fi
+done
+
+DEFAULT_DIR="$HOME/pipeline"
+
+echo ""
+read -p "Enter installation directory [default: $DEFAULT_DIR]: " INSTALL_DIR
+
+
+INSTALL_DIR="${INSTALL_DIR:-$DEFAULT_DIR}"
+
+
+mkdir -p "$INSTALL_DIR"
+
+echo "[+] Ateru will be installed in: $INSTALL_DIR"
+
+
+if [ -d "$INSTALL_DIR/ateru" ]; then
+    echo "[!] Directory $INSTALL_DIR/ateru already exists."
+    read -p "Do you want to overwrite it? [y/N]: " OVERWRITE
+    if [[ "$OVERWRITE" =~ ^[Yy]$ ]]; then
+        rm -rf "$INSTALL_DIR/ateru"
+    else
+        echo "Installation cancelled."
+        exit 1
     fi
 fi
 
-if ! command -v curl >/dev/null 2>&1; then
-    echo "[!] curl not found. Installing..."
-    if [[ "$OS" == "Linux" ]]; then
-        if command -v apt >/dev/null 2>&1; then sudo apt install -y curl; fi
-        if command -v dnf >/dev/null 2>&1; then sudo dnf install -y curl; fi
-    elif [[ "$OS" == "Darwin" ]]; then
-        brew install curl
-    fi
-fi
-
-
-TMP_DIR="$(mktemp -d)"
-echo "[+] Cloning Ateru repo into $TMP_DIR"
-git clone --depth 1 https://github.com/ronnyascencio/ateru.git "$TMP_DIR"
+echo "[+] Cloning Ateru repo..."
+git clone --depth 1 https://github.com/ronnyascencio/ateru.git "$INSTALL_DIR/ateru"
 
 
 echo "[+] Running set_up_unix.sh..."
-bash "$TMP_DIR/scripts/set_up_unix.sh"
-
-
-echo "[+] Cleaning up temporary files..."
-rm -rf "$TMP_DIR"
+bash "$INSTALL_DIR/ateru/scripts/set_up_unix.sh"
 
 echo ""
 echo "========================================="
